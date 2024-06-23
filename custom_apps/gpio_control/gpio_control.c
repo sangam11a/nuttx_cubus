6e1bb3c07fcf3980dbb666dfb18371a8ac94089b
sushant-XD <sdhakal764@gmail.com>
2024-06-23 16:59:14 +0545

    refined gpio application

--- custom_apps/gpio_control/gpio_control.c
+++ custom_apps/gpio_control/gpio_control.c
@@ -37,50 +37,62 @@
  * spi_driver_test_main
  ****************************************************************************/
 
-int gpio_write(uint32_t pin, uint8_t mode){
+int gpio_write(uint32_t pin, uint8_t mode)
+{
 
-    gpio_config_s gpio_numval;
-    int fd = open(ETX_LED_DRIVER_PATH, O_WRONLY);
-    if(fd < 0){
-        syslog(LOG_ERR, "Error opening %s for GPIO WRITE...", ETX_LED_DRIVER_PATH);
-        close(fd);
-        return -1;
-    }
-    gpio_numval.gpio_num = pin;
-    gpio_numval.gpio_val = mode;
-    if(gpio_numval.gpio_val > 1 || gpio_numval.gpio_num < 0){
-        syslog(LOG_ERR,"Undefined GPIO pin or set mode selected...\n");
-        return -2;
-    }
-    int ret = write(fd, (const void *)&gpio_numval, sizeof(gpio_config_s));
+  gpio_config_s gpio_numval;
+  int fd = open(ETX_LED_DRIVER_PATH, O_WRONLY);
+  if (fd < 0)
+  {
+    syslog(LOG_ERR, "Error opening %s for GPIO WRITE...", ETX_LED_DRIVER_PATH);
     close(fd);
-    if(ret < 0 ){
-        syslog(LOG_ERR, "Unable to write to gpio pin...\n");
-    }
-    return ret;
+    return -1;
+  }
+  gpio_numval.gpio_num = pin;
+  gpio_numval.gpio_val = mode;
+  if (gpio_numval.gpio_val > 1 || gpio_numval.gpio_num < 0)
+  {
+    syslog(LOG_ERR, "Undefined GPIO pin or set mode selected...\n");
+    return -2;
+  }
+  int ret = write(fd, (const void *)&gpio_numval, sizeof(gpio_config_s));
+  close(fd);
+  if (ret < 0)
+  {
+    syslog(LOG_ERR, "Unable to write to gpio pin...\n");
+  }
+  return ret;
 }
 
 int main(int argc, FAR char *argv[])
 {
-  printf("Starting GPIO Control application...\n");
-  printf("Pin Name: %s Pin number %d\n", argv[1], atoi(argv[2]));
-  if(argc < 3){
-    printf("Enter both subsystem name and gpio mode\n");
+  if (argc < 3)
+  {
+    printf("Enter both subsystem name and gpio mode\n Enter: Application Name, Subsystem name (CAM, MSN1, MSN2, MSN3 etc.), GPIO Pin Mode (1 or 0)\n");
     return -1;
   }
   uint8_t pin_mode = atoi(argv[2]);
-  if(!strcmp(argv[1], "COM")){
+  if (!strcmp(argv[1], "COM"))
+  {
     gpio_write(GPIO_3V3_COM_EN, pin_mode);
-  }else if(!strcmp(argv[1], "MSN1")){ //ADCS
+  }
+  else if (!strcmp(argv[1], "MSN1"))
+  { // ADCS
     gpio_write(GPIO_MSN_3V3_EN, pin_mode);
     gpio_write(GPIO_MSN1_EN, pin_mode);
-  }else if(!strcmp(argv[1], "MSN2")){ //CAM
+  }
+  else if (!strcmp(argv[1], "MSN2"))
+  { // CAM
     gpio_write(GPIO_MSN_3V3_EN, pin_mode);
     gpio_write(GPIO_MSN2_EN, pin_mode);
-  }else if(!strcmp(argv[1], "MSN3")){ //EPDM
+  }
+  else if (!strcmp(argv[1], "MSN3"))
+  { // EPDM
     gpio_write(GPIO_MSN_3V3_EN, pin_mode);
     gpio_write(GPIO_MSN3_EN, pin_mode);
-  }else{  //keep on adding other gpio pins as you go
+  }
+  else
+  { // keep on adding other gpio pins as you go
     printf("Unknown command \n");
     return -2;
   }
